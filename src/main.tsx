f153fa074764a3e97867df1c7317db92ab5613ba
zuiken-code <zuiken2022robo@gmail.com>
2025-12-30 01:51:19 +0900

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Router basename={import.meta.env.DEV ? "/" : "/レポジトリ名/"}>
-    <App />
-  </Router>
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
