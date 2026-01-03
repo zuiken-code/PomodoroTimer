474a9687e62f8495811e024f55875c744d513f27
zuiken-code <zuiken2022robo@gmail.com>
2026-01-03 20:32:43 +0900

    fix css bug

--- src/components/TimerPanel.tsx
+++ src/components/TimerPanel.tsx
@@ -16,37 +16,38 @@ export function TimerPanel({
   stopTimer,
 }: props) {
   return (
-    <div className="timer-panel">
-      {timer.mode !== "stop" && (
-        <div
-          style={{
-            padding: "8px 16px",
-            borderRadius: "8px",
-            display: "inline-block",
-            margin: "10px 0",
-            fontWeight: "bold",
-            // モードによって背景色と文字色を変える
-            backgroundColor: timer.mode === "work" ? "#ffebee" : "#e8f5e9",
-            color: timer.mode === "work" ? "#d32f2f" : "#2e7d32",
-            border: `1px solid ${
-              timer.mode === "work" ? "#ffcdd2" : "#c8e6c9"
-            }`,
-          }}
-        >
-          {timer.mode === "work" ? "🚀 集中タイム" : "☕ 休憩タイム"}
-        </div>
-      )}
+    <>
+      <div className="timer-panel">
+        {timer.mode !== "stop" && (
+          <div
+            style={{
+              padding: "8px 16px",
+              borderRadius: "8px",
+              display: "inline-block",
+              margin: "10px 0",
+              fontWeight: "bold",
+              // モードによって背景色と文字色を変える
+              backgroundColor: timer.mode === "work" ? "#ffebee" : "#e8f5e9",
+              color: timer.mode === "work" ? "#d32f2f" : "#2e7d32",
+              border: `1px solid ${
+                timer.mode === "work" ? "#ffcdd2" : "#c8e6c9"
+              }`,
+            }}
+          >
+            {timer.mode === "work" ? "🚀 集中タイム" : "☕ 休憩タイム"}
+          </div>
+        )}
 
-      {timer.targetTime && (
-        <FlipClockCountdown
-          to={timer.targetTime}
-          renderMap={[false, false, true, true]}
-          onComplete={setFinished}
-        />
-      )}
-
+        {timer.targetTime && (
+          <FlipClockCountdown
+            to={timer.targetTime}
+            renderMap={[false, false, true, true]}
+            onComplete={setFinished}
+          />
+        )}
+      </div>
       <button onClick={startTimer}>スタート</button>
       <button onClick={stopTimer}>やめる</button>
-    </div>
+    </>
   );
 }
