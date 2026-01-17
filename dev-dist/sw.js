7126909b9c5da8aef7242a1c7cf8f722999a410b
zuiken-code <zuiken2022robo@gmail.com>
2026-01-17 14:31:50 +0900

    add infomation

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "f5e321d3e126c7badc0517b96da07717"
   }, {
     "url": "index.html",
-    "revision": "0.ppd0htk7hgg"
+    "revision": "0.h2eoq77ge4o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
