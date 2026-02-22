369fc94751bdb091652debb348dde830e8ec90f3
zuiken-code <zuiken2022robo@gmail.com>
2026-02-22 21:16:33 +0900

    update css

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "f5e321d3e126c7badc0517b96da07717"
   }, {
     "url": "index.html",
-    "revision": "0.i7dr52p3u44"
+    "revision": "0.vqsiu23kgvc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
